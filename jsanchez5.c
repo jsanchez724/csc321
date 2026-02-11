f4ed3311056d0dd12435b331a8ca05b28bd9b05b
js <jsanchez724@toromail.csudh.edu>
2026-02-11 02:04:36 +0000

    Lab #5 Test Version

---
+++ jsanchez5.c
@@ -0,0 +1,37 @@
+//Joan Sanchez Lopez
+//CSC321
+//Lab 5
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(void)
+{
+	int x = 5;
+
+	if(x  > 2)
+	{
+		printf("X is greater than 2 \n");
+
+
+	}
+	else 
+	{
+		puts("X is less than 2");
+
+
+	}
+
+	if (x != 5)
+	{
+		int y = 0;
+	}
+
+	printf("Printing y = %d \n",y);
+
+	return EXIT_SUCCESS;
+
+}
+

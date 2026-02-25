00f0f9475a75fe9d99adbf7f1bb88ffefa62b1dc
js <jsanchez724@toromail.csudh.edu>
2026-02-25 01:44:49 +0000

    Lab #7 Python First Version Sample

---
+++ jsanchez7.py
@@ -0,0 +1,15 @@
+#Joan Sanchez
+#Lab #7
+#CSC321
+
+x = 29
+
+if x > 2:
+    print("x is greater than 2")
+elif x < 2:
+    print("x is less than 2 while the value of x is",x)
+else:
+    print("x is ",x," which is equal to 2")
+
+
+

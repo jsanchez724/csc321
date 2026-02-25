a986ffd6035643411ecd96f542bce33f1be682b8
js <jsanchez724@toromail.csudh.edu>
2026-02-25 01:55:07 +0000

    Lab #7 Python First Version

--- jsanchez7.py
+++ jsanchez7.py
@@ -11,5 +11,19 @@ elif x < 2:
 else:
     print("x is ",x," which is equal to 2")
 
+y = 20.1
 
+if y > 21.2:
+    print("y is greater than 20.1")
+elif y < 21.2:
+    print("y is less than than 20.1")
+else:
+    print("y is ",y," which is equal to 20.1")
+
+z = "bumblebee"
+
+if z == "bumblebee":
+    print("variable z is indeed a bumblebee")
+else:
+    print("variable z is not bumblebee")
 

98c8deb81b25cb9433ab488b0eaac54331a66355
js <jsanchez724@toromail.csudh.edu>
2026-03-24 06:42:17 +0000

    Lab #9 Jave Version 1.0

---
+++ jsanchez9.java
@@ -0,0 +1,85 @@
+//Joan Sanchez
+//CSC321
+//Lab #9
+
+
+import java.util.Scanner;
+import java.lang.Math;
+
+
+
+public class jsanchez9
+{
+	public static void main(String [] args)
+	{
+
+		int max = 20, min = 1;
+		int randomNum = (int)((Math.random() * (max - min)) + min);
+		int userNumChoice = 0;
+		int i;
+		int counter = 0;
+		int maxAttempts = 10;
+
+
+		Scanner input = new Scanner(System.in);
+
+
+		System.out.println("You have 10 tries to guess the correct number.");
+
+		while(userNumChoice != randomNum)
+		{
+			System.out.println("Please enter a number between 1 - 20: ");
+			userNumChoice = input.nextInt();
+
+
+
+
+			if(userNumChoice >= 1 && userNumChoice <= 20 )
+			{
+			
+				if(userNumChoice == randomNum)
+				{
+					System.out.println("You have gussed correctly, the number was: " + randomNum);
+					for(i = 0; i < counter; i++)
+					{
+						System.out.println("You won and guessed the number correctly which was: " + randomNum);
+
+					}
+
+					break;
+				}
+
+			}
+			else
+			{
+				System.out.println("Your input exceeded our program processing capabilities. Please enter a valid number between 1 - 20: ");
+
+			}
+
+			if(userNumChoice < randomNum)
+			{
+				System.out.println("The number you have guessed is lower than the winning number, Please try again.");
+
+			} 
+			else if(userNumChoice > randomNum)
+			{
+				System.out.println("The numberyou have guessed is greater than the winning number, Please try again");
+			}
+
+			if(counter == maxAttempts)
+			{
+
+				System.out.println("You have used up all your guesses, the number was: " + randomNum);
+				break;
+
+			}
+			counter++;
+
+
+		}
+
+
+	}
+
+
+}
